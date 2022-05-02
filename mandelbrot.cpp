01740f852629eceb9ce8ecbb97fe9e26ed6f99ec
Alexander Paniman <alexpaniman@gmail.com>
2022-05-03 00:46:58 +0300

    feature: implement zoom and motion

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -16,14 +16,14 @@
 class mandelbrot_window: public gl::window {
 private:
     struct colored_vertex final {
-        math::vec<float, 2> point;
-        math::vec<float, 3> color;
+        math::vec<double, 2> point;
+        math::vec<double, 3> color;
 
         colored_vertex()
-            : point(0.0f, 0.0f), color(0.0f, 0.0f, 0.0f) {};
+            : point(0.0, 0.0), color(0.0, 0.0, 0.0) {};
 
-        colored_vertex(math::vec<float, 2> new_point,
-                       math::vec<float, 3> new_color)
+        colored_vertex(math::vec<double, 2> new_point,
+                       math::vec<double, 3> new_color)
             : point(new_point), color(new_color) {};
     };
 
@@ -31,41 +31,32 @@ private:
     gl::shaders::shader_program colored_renderer;
 
 
-    static inline float power2(float value) {
-        return value * value;
-    }
-
-    static inline float dot(math::vec<float, 2> vec) {
-        return power2(vec.x()) + power2(vec.y());
-    }
-
-    math::vec<float, 3> calculate_mandelbrot_point(math::vec<float, 2> point) {
-        math::vec<float, 2> iteration(0.0f, 0.0f);
+    math::vec<double, 3> calculate_mandelbrot_point(math::vec<double, 2> point) {
+        math::vec z(0.0, 0.0);
 
         for (int i = 0; i < 128; ++ i) {  
-            iteration = math::vec(power2(iteration.x()) - power2(iteration.y()),
-                                  2.0f * iteration.x() * iteration.y());
+            z = math::vec(math::pow(z.x(), 2) - math::pow(z.y(), 2),
+                          2.0 * z.x() * z.y()) + point;
 
-            iteration.x() += point.x(); iteration.y() += point.y();
-            if (dot(iteration) > 4.0f)
-                return math::vec((float) sin(0.5f * i) + 0.4f,
-                                 (float) sin(i + 5.0f) + 0.4f,
-                                 (float) cos(i * 0.4f) + 0.4f);
+            if (z.len() > 4.0)
+                return math::vec((double) sin(0.5 * i) + 0.4,
+                                 (double) sin(i + 5.0) + 0.4,
+                                 (double) cos(i * 0.4) + 0.4);
         }
 
-        return math::vec(0.0f, 0.0f, 0.0f);
+        return math::vec(0.0, 0.0, 0.0);
     }
 
-    void draw_mandelbrot(math::vec<float, 2> position, const double zoom) {
+    void draw_mandelbrot(math::vec<double, 2> position, const double zoom) {
         for (size_t i = 0; i < height; ++ i)
             for (size_t j = 0; j < width; ++ j) {
                 colored_vertex& current = points[i * width + j];
 
                 // Current point's coordinates:
-                current.point = math::vec(j * 2.0f / width - 1.0f, i * 2.0f / height - 1.0f);
+                current.point = math::vec(j * 2.0 / width - 1.0, i * 2.0 / height - 1.0);
 
-                math::vec<float, 2> mandelbrot_point =
-                    current.point * math::vec(width / (float) height, 1.0f) * zoom + position;
+                math::vec<double, 2> mandelbrot_point =
+                    current.point * math::vec(width / (double) height, 1.0) * zoom + position;
 
                 // Current point's color:
                 current.color = calculate_mandelbrot_point(mandelbrot_point);
@@ -74,12 +65,42 @@ private:
         points.update();
     }
 
+    double zoom = 1.4;
+    math::vec<double, 2> position { -0.5, 0.0 };
+
 public:
     using gl::window::window;
 
+    void on_key_pressed(key pressed_key) override {
+        switch (pressed_key) {
+        case gl::window::key::KEY_KP_ADD:
+        case gl::window::key::KEY_EQUAL : zoom /= 1.2;                              break;
+        case gl::window::key::KEY_MINUS : zoom *= 1.2;                              break;
+
+        case gl::window::key::KEY_DOWN  :
+        case gl::window::key::KEY_S     :
+        case gl::window::key::KEY_J     : position += math::vec(+0.0, -0.3) * zoom; break;
+
+        case gl::window::key::KEY_UP    :
+        case gl::window::key::KEY_W     :
+        case gl::window::key::KEY_K     : position += math::vec(+0.0, +0.3) * zoom; break;
+
+        case gl::window::key::KEY_LEFT  :
+        case gl::window::key::KEY_H     :
+        case gl::window::key::KEY_A     : position += math::vec(-0.3, +0.0) * zoom; break;
+
+        case gl::window::key::KEY_RIGHT :
+        case gl::window::key::KEY_L     :
+        case gl::window::key::KEY_D     : position += math::vec(+0.3, +0.0) * zoom; break;
+
+        default:
+            break;
+        }
+    }
+
     void setup() override {
-        points.set_layout(math::vector_layout<float, 2>() +
-                          math::vector_layout<float, 3>());
+        points.set_layout(math::vector_layout<double, 2>() +
+                          math::vector_layout<double, 3>());
 
         // Fill with point for each pixel:
         points.resize((size_t) width * (size_t) height);
@@ -92,7 +113,7 @@ public:
     }
 
     void draw() override {
-        draw_mandelbrot(math::vec(-0.5f, 0.0f), 1.4f);
+        draw_mandelbrot(position, zoom);
         gl::draw(gl::drawing_type::POINTS, points, colored_renderer);
     }
 };
